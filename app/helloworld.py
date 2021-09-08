067f2e477a50490d574f0996bbfba6931d17fb60
ablitsman <ablitsman@chegg.com>
2021-09-08 17:48:42 +0300

    Adding logger

--- app/helloworld.py
+++ app/helloworld.py
@@ -1,10 +1,18 @@
 from flask import Flask
+import logging
+
+from opencensus.ext.azure.log_exporter import AzureEventHandler
+
+logger = logging.getLogger(__name__)
+logger.addHandler(AzureEventHandler(connection_string='InstrumentationKey=f4427b7f-a37c-4e89-a629-1c270edea42c'))
+logger.setLevel(logging.INFO)
+logger.info('Hello, World!')
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return 'Hello, World!'
 
 # Needed only for local testing - gunicorn (Python Process Manager) takes care for it in real-time
 #app.run(host='localhost', port=8080)
