10becb7de84f60717e55fe711e1aefed4ed430c6
ablitsman <ablitsman@chegg.com>
2021-09-08 20:01:41 +0300

    Fixed Azure Insights

--- app/helloworld.py
+++ app/helloworld.py
@@ -1,19 +1,31 @@
 from flask import Flask
+from logging import StreamHandler
 import logging
 
-from opencensus.ext.azure.log_exporter import AzureEventHandler
+from applicationinsights.flask.ext import AppInsights
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+
+
+## Logger Format
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 logger = logging.getLogger(__name__)
-logger.addHandler(AzureEventHandler(connection_string='InstrumentationKey=f4427b7f-a37c-4e89-a629-1c270edea42c'))
-logger.setLevel(logging.INFO)
-
+logger.addHandler(AzureLogHandler(connection_string='InstrumentationKey=f4427b7f-a37c-4e89-a629-1c270edea42c'))
 
 app = Flask(__name__)
 
+# keep stdout/stderr logging using StreamHandler - for testing purposes
+#streamHandler = StreamHandler()
+#app.logger.addHandler(streamHandler)
+
 @app.route('/')
 def index():
-    logger.info('Hello World!')
+    logger.warning('Hello, World!')
     return 'Hello World!'
+
 
 # Needed only for local testing - gunicorn (Python Process Manager) takes care for it in real-time
 #app.run(host='localhost', port=8080)
